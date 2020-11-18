a2c0b4b712cdbaa23109f01aa7418854f30b2a88
Maria Frazão <maria_frazao@hotmail.com>
2020-11-18 10:26:13 +0000

    initProject

---
+++ CountriesMobileApp/CountriesMobileApp/Services/IApiService.cs
@@ -0,0 +1,13 @@
+﻿using CountriesMobileApp.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountriesMobileApp.Services
+{
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller);
+    }
+}
---
+++ CountriesMobileApp/CountriesMobileApp/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CountriesMobileApp.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
